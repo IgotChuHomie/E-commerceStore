b3328d642270a0eba09af5762af38e1746384a61
IgotChuHomie <abdouneoussama592@gmail.com>
2022-05-21 17:26:23 +0100

    creating Router for each page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  About,
+  SingleProduct,
+  Products,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <>
+      <Router>
+        <Navbar></Navbar>
+        <Sidebar></Sidebar>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/aboute">
+            <About></About>
+          </Route>
+          <Route exact path="/cart">
+            <Cart></Cart>
+          </Route>
+          <Route exact path="/products">
+            <Products></Products>
+          </Route>
+          <Route exact path="/products/:id" children={SingleProduct}></Route>
+          <Route exact path="/checkout">
+            <Checkout></Checkout>
+          </Route>
+          <Route path="*">
+            <Error></Error>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import SingleProduct from "./SingleProductPage";
+import Products from "./ProductsPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
+
+export {
+  Home,
+  About,
+  SingleProduct,
+  Products,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+};
