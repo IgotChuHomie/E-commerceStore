c197584b965a31386c6bc8793201b08249a9c8b2
IgotChuHomie <abdouneoussama592@gmail.com>
2022-05-25 12:54:35 +0100

    setting up the about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about"></PageHero>
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="About Image" />
+        <article>
+          <div className="title">
+            <h2>Our Story</h2>
+            <div className="underline"></div>
+            <article>
+              ti recusandae accusamus eum assumenda illo alias maxime non
+              consequuntur quisquam, nam vero ullam. Facilis consequuntur
+              molestias inventore porro quis eius officiis quam veniam
+              consectetur! Ducimus, omnis! In nisi nobis sequi praesentium dolor
+              odit, ut quisquam commodi corporis voluptate deserunt aut animi
+              placeat. Corporis, ipsam.
+            </article>
+          </div>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +54,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
