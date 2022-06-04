c9b2bb452155d4e4aceb0e23c1ac9dabfe6a7900
IgotChuHomie <abdouneoussama592@gmail.com>
2022-06-04 18:33:59 +0100

    Add to Cart - Existing item

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -13,6 +13,19 @@ const cart_reducer = (state, action) => {
       return i.id === id + color;
     });
     if (tempItem) {
+      const tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === id + color) {
+          let newAmount = cartItem.amount + amount;
+          if (newAmount > cartItem.max) {
+            newAmount = cartItem.max;
+          }
+          return { ...cartItem, amount: newAmount };
+        } else {
+          return cartItem;
+        }
+      });
+
+      return { ...state, cart: tempCart };
     } else {
       const newItem = {
         id: id + color,
