afefd52c20bfd5ac6cffad991ba1e5cb39138050
IgotChuHomie <abdouneoussama592@gmail.com>
2022-06-04 22:49:06 +0100

    Cart content component

--- src/components/CarContent.js
+++ src/components/CarContent.js
@@ -7,7 +7,30 @@ import CartItem from "./CartItem";
 import CartTotals from "./CartTotals";
 
 const CartContent = () => {
-  return <h4>cart content </h4>;
+  const { cart, clearCart } = useCartContext();
+
+  return (
+    <Wrapper className="section section-center">
+      <CartColumns />
+      {cart.map((item) => {
+        return <CartItem key={item.id} {...item}></CartItem>;
+      })}
+      <hr />
+      <div className="link-container">
+        <Link to="/products" className="link-btn">
+          continue shopping
+        </Link>
+        <button
+          type="button"
+          className="link-btn clear-btn"
+          onClick={clearCart}
+        >
+          Clear Cart
+        </button>
+      </div>
+      <CartTotals />
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
