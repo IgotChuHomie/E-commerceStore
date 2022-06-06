fd53709dc347e4bf3a1e165367be6e3f533d120c
IgotChuHomie <abdouneoussama592@gmail.com>
2022-06-06 17:18:59 +0100

    Modify the dependencies

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,15 +1,37 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import { StripeCheckout } from "../components";
+// extra imports
+import { useCartContext } from "../context/cart_context";
+import { Link } from "react-router-dom";
+
 const CheckoutPage = () => {
+  const { cart } = useCartContext();
   return (
     <main>
-      <PageHero title='checkout' />
-      <Wrapper className='page'>
-        <h1>checkout here</h1>
+      <PageHero title="checkout" />
+      <Wrapper className="page">
+        {cart.length < 1 ? (
+          <div className="empty">
+            <h2>your cart is empty </h2>
+            <Link to="/products" className="btn">
+              Fill it
+            </Link>
+          </div>
+        ) : (
+          <StripeCheckout />
+        )}
       </Wrapper>
     </main>
-  )
-}
-const Wrapper = styled.div``
-export default CheckoutPage
+  );
+};
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .empty {
+    text-align: center;
+  }
+`;
+export default CheckoutPage;
