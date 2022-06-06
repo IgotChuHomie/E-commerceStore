eb4f7623d7b48a17de833e33ec07c7f0805f78ee
IgotChuHomie <abdouneoussama592@gmail.com>
2022-06-06 13:35:43 +0100

    add link to all products in featured products

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -29,6 +29,9 @@ const FeaturedProducts = () => {
           return <Product key={product.id} {...product} />;
         })}
       </div>
+      <Link to="/products" className="btn">
+        All products
+      </Link>
     </Wrapper>
   );
 };
