c338676787ad795e90d4bfc28e5315a3f46f77c7
IgotChuHomie <abdouneoussama592@gmail.com>
2022-06-07 15:46:53 +0100

    setting up the netlify serverless

--- functions/create-payement-intent.js
+++ functions/create-payement-intent.js
@@ -4,10 +4,18 @@
  * @param {*} context
  * @returns object
  */
- exports.handler = async function (event, context) {
+exports.handler = async function (event, context) {
+  if (event.body) {
+    const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
+    console.log(cart);
     return {
       statusCode: 200,
-      body: "hello",
+      body: JSON.stringify(cart),
     };
-  };
-  
+  } else {
+    return {
+      statusCode: 200,
+      body: "please create payement",
+    };
+  }
+};
