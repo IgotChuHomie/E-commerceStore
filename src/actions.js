4609958dbbf5ac1d23e111c81b471b17a644cae3
RaiseenCole <86480241+RaiseenCole@users.noreply.github.com>
2022-07-14 16:28:48 +0100

    Update actions.js
    Adding the Test status to the actions

--- src/actions.js
+++ src/actions.js
@@ -19,3 +19,4 @@ export const REMOVE_CART_ITEM = "REMOVE_CART_ITEM";
 export const TOGGLE_CART_ITEM_AMOUNT = "TOGGLE_CART_ITEM_AMOUNT";
 export const CLEAR_CART = "CLEAR_CART";
 export const COUNT_CART_TOTALS = "COUNT_CART_TOTALS";
+export const TESTING_CARD = true ; 
